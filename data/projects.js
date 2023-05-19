7fdfa02fe7b06bff7236f45a5949523aa9a2999e
GoorezyEST <goorezyestudios@gmail.com>
2023-05-18 23:08:57 -0300

    Added better project images but probably not the final ones

--- data/projects.js
+++ data/projects.js
@@ -1,37 +1,37 @@
 export const data = [
   {
     text: "PC F2P: Un sitio web para buscar juegos gratuitos.",
-    image: "https://i.imgur.com/Mwwv7tr.png",
+    image: "https://i.imgur.com/CK6vTfm.png",
     alt: "PC F2P Website image",
     link: "https://pc-f2p.netlify.app/",
   },
   {
     text: "Mate y código: Mi blog de tecnología y programación.",
-    image: "https://i.imgur.com/9T0k1Ld.png",
+    image: "https://i.imgur.com/xxDVusi.png",
     alt: "Mate y código Website image",
     link: "https://mate-y-codigo.vercel.app/",
   },
   {
     text: "Hawk Clothes: Ejemplo de sitio web e-commerce.",
-    image: "https://i.imgur.com/vDFMoYR.png",
+    image: "https://i.imgur.com/P9nw3Rg.png",
     alt: "Hawk Clothes Website image",
     link: "https://hawk-clothes.netlify.app/home",
   },
   {
     text: "Geek Genius: Bot de trivias para Discord con .NET.",
-    image: "https://i.imgur.com/Cgq2N83.png",
+    image: "https://i.imgur.com/fkL1WkO.png",
     alt: "Geek Genius Discord Bot image",
     link: "https://www.linkedin.com/posts/franco-espinosa_netdeveloper-developer-desarrollodesoftware-activity-7051901781492019200-wGz0/?utm_source=share&utm_medium=member_desktop",
   },
   {
     text: "Portafolio I: Este es mi portafolio previo en React.",
-    image: "https://i.imgur.com/tHB4hWe.png",
+    image: "https://i.imgur.com/dZTV03E.png",
     alt: "Portafolio I Website image",
     link: "https://goorezy-portfolio.netlify.app/",
   },
   {
     text: 'CSS Gallery: Una galeria de efectos "only css".',
-    image: "https://i.imgur.com/7gD5IUS.png",
+    image: "https://i.imgur.com/Cq90PGn.png",
     alt: "CSS Gallery Website image",
     link: "https://goorezy-css-gallery.netlify.app/",
   },
