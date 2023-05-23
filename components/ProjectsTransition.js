c147df438a7a493a55fca67e6bc23050dea47cbd
GoorezyEST <goorezyestudios@gmail.com>
2023-05-23 16:43:53 -0300

    Added scaling animation to dots

--- components/ProjectsTransition.js
+++ components/ProjectsTransition.js
@@ -8,8 +8,8 @@ export default function ProjectsTransition() {
       <div className={styles.transition}>
         <motion.div
           className={styles.dots}
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
+          initial={{ opacity: 0, scale: 0, x: "-50%" }}
+          whileInView={{ opacity: 1, scale: 1, x: "-50%" }}
           viewport={false}
           transition={{
             duration: 0.5,
