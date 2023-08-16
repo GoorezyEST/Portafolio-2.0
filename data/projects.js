2330b60a3077cdbf0214fb3a024e5ca985737a1b
GoorezyEST <goorezyestudios@gmail.com>
2023-08-16 20:21:53 -0300

    Modified projects

--- data/projects.js
+++ data/projects.js
@@ -1,37 +1,37 @@
 export const data = [
   {
+    text: "E-Booking: Ejemplo sitio web reservas electrónicas.",
+    image: "https://i.imgur.com/gcnmkMy.png",
+    alt: "E-Booking Website image",
+    link: "https://electronic-booking.vercel.app/",
+  },
+  {
     text: "PC F2P: Un sitio web para buscar juegos gratuitos.",
-    image: "https://i.imgur.com/u4V0cll.png",
+    image: "https://i.imgur.com/kV0nqxd.png",
     alt: "PC F2P Website image",
     link: "https://pc-f2p.netlify.app/",
   },
   {
     text: "Mate y código: Mi blog de tecnología y programación.",
-    image: "https://i.imgur.com/rogb8DI.png",
+    image: "https://i.imgur.com/MGyyn8J.png",
     alt: "Mate y código Website image",
     link: "https://mate-y-codigo.vercel.app/",
   },
   {
-    text: "Hawk Clothes: Ejemplo de sitio web e-commerce.",
-    image: "https://i.imgur.com/HpumRgD.png",
-    alt: "Hawk Clothes Website image",
-    link: "https://hawk-clothes.netlify.app/home",
-  },
-  {
     text: "Geek Genius: Bot de trivias para Discord con .NET.",
-    image: "https://i.imgur.com/LuV6ixg.png",
+    image: "https://i.imgur.com/KlMvklZ.png",
     alt: "Geek Genius Discord Bot image",
     link: "https://www.linkedin.com/posts/franco-espinosa_netdeveloper-developer-desarrollodesoftware-activity-7051901781492019200-wGz0/?utm_source=share&utm_medium=member_desktop",
   },
   {
     text: "Portafolio I: Este es mi portafolio previo en React.",
-    image: "https://i.imgur.com/69dZtqZ.png",
+    image: "https://i.imgur.com/ww03ARD.png",
     alt: "Portafolio I Website image",
     link: "https://franco-espinosa-portafolio-antiguo.netlify.app/",
   },
   {
     text: 'CSS Gallery: Una galeria de efectos "only css".',
-    image: "https://i.imgur.com/fkxIQ1W.png",
+    image: "https://i.imgur.com/l9OJsZp.png",
     alt: "CSS Gallery Website image",
     link: "https://goorezy-css-gallery.netlify.app/",
   },
